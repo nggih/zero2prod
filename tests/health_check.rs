e86e65b0a57460137516dabc4fe693fa5ecefda8
nggih <linggih.saputro@sci.ui.ac.id>
2023-03-26 22:30:37 +0700

    chapter 3: add tests

---
+++ tests/health_check.rs
@@ -0,0 +1,26 @@
+//! tests/health_check.rs
+
+// use zero2prod::main;
+#[tokio::test]
+async fn health_check_works() {
+    // Arrange 
+    spawn_app().await.expect("Failed to spawn our app.");
+    // We need to bring in 'reqwest'
+    // to perform HTTP requests against our application
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client 
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+async fn spawn_app() -> Result<(), std::io::Error> {
+    zero2prod::run().await
+}
