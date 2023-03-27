5151353d1733fbf6394ab00e26da07c2ce709f37
nggih <linggih.saputro@sci.ui.ac.id>
2023-03-27 18:25:00 +0700

    last chap 3.7.2 page 41

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,20 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
-use actix_web::dev::Server;
+use std::net::TcpListener;
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
+use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+use actix_web::dev::Server;
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run();
-
+        })
+        // .bind(address)?
+        .listen(listener)?
+        .run();
     // .await removed
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,27 +1,35 @@
 //! tests/health_check.rs
 
+use std::{net::TcpListener};
+
+fn spawn_app() -> String {
+    // let server = zero2prod::run("127.0.0.1:0").expect("Failed to bind address");
+    // let _ = tokio::spawn(server);
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // we retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+    // we return the application address to the caller!
+    format!("http://127.0.0.1:{}", port)
+}
 // use zero2prod::main;
 #[tokio::test]
 async fn health_check_works() {
     // Arrange 
-    spawn_app();
+    let address = spawn_app();
     // We need to bring in 'reqwest'
     // to perform HTTP requests against our application
     let client = reqwest::Client::new();
 
     // Act
     let response = client 
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
 
     // Assert
     assert!(response.status().is_success());
-    assert_eq!(Some(19), response.content_length());
-}
-
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
-    let _ = tokio::spawn(server);
+    assert_eq!(Some(0), response.content_length());
 }
